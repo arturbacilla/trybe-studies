863bbb12a34a34575b12e8c81007f564605ca8fb
arturbacilla <artur.bacilla1@gmail.com>
2021-07-28 18:57:40 -0300

    Exercicio 04.01-1: Created basic mathematical operators scripts

---
+++ [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/divisao.js
@@ -0,0 +1,9 @@
+let a = null;
+let b = null;
+let divisao;
+
+a = 1;
+b = 2;
+
+divisao = a/b;
+console.log(divisao);
---
+++ [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/subtracao.js
@@ -0,0 +1,9 @@
+let a = null;
+let b = null;
+let subtracao;
+
+a = 1;
+b = 2;
+
+subtracao = a - b;
+console.log(subtracao);
