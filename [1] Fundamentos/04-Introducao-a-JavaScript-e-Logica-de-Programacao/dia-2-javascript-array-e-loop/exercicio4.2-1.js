957864e8599cf4f59be974a0dd8a96bab2b8a328
arturbacilla <artur.bacilla1@gmail.com>
2021-07-29 19:47:18 -0300

    Exercicio 4.2-2: Soma total

--- [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop/exercicio4.2-1.js
+++ [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop/exercicio4.2-1.js
@@ -1,5 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let soma = 0;
 for (let numb of numbers) {
   console.log(numb);
+  soma = soma + numb;
 }
+
+console.log("soma total: " + soma);
