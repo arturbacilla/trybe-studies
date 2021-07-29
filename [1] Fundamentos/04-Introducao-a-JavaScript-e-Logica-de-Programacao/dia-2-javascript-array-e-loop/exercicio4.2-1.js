0c7df8a174dfcbf28add574681244aa45c3004f6
arturbacilla <artur.bacilla1@gmail.com>
2021-07-29 19:58:52 -0300

    Exercicio 4.2-3: Média Aritimética

--- [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop/exercicio4.2-1.js
+++ [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop/exercicio4.2-1.js
@@ -1,9 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let soma = 0;
+let avg = 0;
+
 for (let numb of numbers) {
   console.log(numb);
   soma = soma + numb;
 }
 
+avg = soma/(numbers.length);
+
 console.log("soma total: " + soma);
+console.log("média aritimética: " + avg);
