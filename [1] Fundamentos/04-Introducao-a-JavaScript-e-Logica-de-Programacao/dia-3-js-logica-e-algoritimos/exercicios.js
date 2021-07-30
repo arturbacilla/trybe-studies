2cf9f0d3dce8be49ba2d44eb0d67619dd5f1b128
arturbacilla <artur.bacilla1@gmail.com>
2021-07-30 18:13:25 -0300

    Exercicio 4.3-3: Triângulo de asteriscos invertido

--- [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-3-js-logica-e-algoritimos/exercicios.js
+++ [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-3-js-logica-e-algoritimos/exercicios.js
@@ -4,13 +4,19 @@ if (n <= 1){
   console.log("N precisa ser maior que 2");
 }else{
   // faz um quadrado de nxn
-  for (let i = 1; i < n; i += 1){
+  for (let i = 1; i < n+1; i += 1){
     console.log("*".repeat(n));
-  }
+  };
   console.log();
   // faz um triângulo equilátero de n
-  for (let j = 1; j < n; j += 1){
+  for (let j = 1; j < n+1; j += 1){
     console.log("*".repeat(j));
-  }
+  };
+  console.log();
+  // faz um triângulo equilátero de n invertido
+  for (let k = 1; k < n+1; k += 1){
+    console.log(" ".repeat(n-k) + "*".repeat(k));
+  };
+
 
 }
