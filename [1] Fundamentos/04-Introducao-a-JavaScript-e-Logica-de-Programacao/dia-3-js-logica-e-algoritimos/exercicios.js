ceb88d4f5c6b8e0aba52a36f311651e6bb4e356c
arturbacilla <artur.bacilla1@gmail.com>
2021-07-30 18:08:45 -0300

    Exercicio 4.3-2: Triângulo de asteriscos

--- [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-3-js-logica-e-algoritimos/exercicios.js
+++ [1] Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-3-js-logica-e-algoritimos/exercicios.js
@@ -4,7 +4,13 @@ if (n <= 1){
   console.log("N precisa ser maior que 2");
 }else{
   // faz um quadrado de nxn
-  for (let i = 0; i < n; i += 1){
-      console.log("*".repeat(n));
+  for (let i = 1; i < n; i += 1){
+    console.log("*".repeat(n));
   }
+  console.log();
+  // faz um triângulo equilátero de n
+  for (let j = 1; j < n; j += 1){
+    console.log("*".repeat(j));
+  }
+
 }
