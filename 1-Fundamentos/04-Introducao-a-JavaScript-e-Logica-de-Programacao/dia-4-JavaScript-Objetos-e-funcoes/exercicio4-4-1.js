af0e4ca019c8cd9df946cfe46a7ec22c7296a44f
arturbacilla <artur.bacilla1@gmail.com>
2021-08-02 18:22:56 -0300

    Exercicio 4.4-1.3 - Show all keys

--- 1-Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-4-JavaScript-Objetos-e-funcoes/exercicio4-4-1.js
+++ 1-Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-4-JavaScript-Objetos-e-funcoes/exercicio4-4-1.js
@@ -2,6 +2,11 @@ let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
 };
 
 console.log("Bem vinda, " + info.personagem);
+
+for (chaves in info){
+  console.log(chaves);
+};
