2243b33fae521fc79ceab5c8b51aca30a9759c7a
arturbacilla <artur.bacilla1@gmail.com>
2021-08-11 18:19:30 -0300

    Exercicio 5.3-1: Populated days of month

--- 1-Fundamentos/05-Introducao-JavaScript-Projetos/dia-3-JavaScript-Eventos/exercises.js
+++ 1-Fundamentos/05-Introducao-JavaScript-Projetos/dia-3-JavaScript-Eventos/exercises.js
@@ -14,3 +14,15 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+function createDaysOfMonth() {
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const daysUList = document.querySelector('#days');
+
+for (d in dezDaysList) {
+  const daysLi = document.createElement('li');
+  daysLi.innerHTML = dezDaysList[d];
+  daysUList.appendChild(daysLi);
+};
+};
+
+createDaysOfMonth();
