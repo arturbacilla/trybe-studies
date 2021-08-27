09ffad1bd06cd769bb9e64e2f446f086ce0acd81
arturbacilla <artur.bacilla1@gmail.com>
2021-08-27 18:14:35 -0300

    Parte I.2 started

--- 1-Fundamentos/07-JavaScript-ES6-e-Testes-Unitarios/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script.js
+++ 1-Fundamentos/07-JavaScript-ES6-e-Testes-Unitarios/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script.js
@@ -24,3 +24,9 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+console.log(oddsAndEvens); // será necessário alterar essa linha �
