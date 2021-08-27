1054acbf7d5abdb5d54e24acdd98a00dee512bc3
arturbacilla <artur.bacilla1@gmail.com>
2021-08-27 18:46:11 -0300

    Parte II started

--- 1-Fundamentos/07-JavaScript-ES6-e-Testes-Unitarios/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script.js
+++ 1-Fundamentos/07-JavaScript-ES6-e-Testes-Unitarios/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,17 +1,4 @@
-// function testingScope(escopo) {
-//   if (escopo === true) {
-//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-//     console.log(ifScope);
-//   } else {
-//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//     console.log(elseScope);
-//   }
-//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-// }
-
-// testingScope(true);
-
+// PARTE I
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -33,3 +20,5 @@ const sortedArray = oddsAndEvens.sort(function (a,b) {
 });
 
 console.log(`Os números ${sortedArray.join()} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha �
+
+//PARTE 2
